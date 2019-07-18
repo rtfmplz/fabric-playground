723105e16b2b84c958effb1e06999c7d820c38c7
jae <kjlee.ko@gmail.com>
2019-07-19 00:22:17 +0900

    update script/clean.sh

--- script/clean.sh
+++ script/clean.sh
@@ -3,8 +3,10 @@
 rm -rf *.block
 rm -rf *.tx
 rm -rf crypto
-rm -rf ./fabcar/hfc-key-store
-#rm -rf ./fabcar/node_modules
+rm -rf ./dapps/fabcar/wallet
+#rm -rf ./dapps/fabcar/node_modules
+rm -rf ./dapps/example02/wallet
+#rm -rf ./dapps/example02/node_modules
 rm -rf ./production
 docker rmi $(docker images | grep dev)
 
