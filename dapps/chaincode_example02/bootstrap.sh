7452f419e507c02ac0b677abb8b234b36a975a30
jae <kjlee.ko@gmail.com>
2019-08-20 18:22:55 +0900

    connection.json에 grpcOptions/ssl-target-name-override 추가

--- dapps/chaincode_example02/bootstrap.sh
+++ dapps/chaincode_example02/bootstrap.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
+#########################################################
+# Variables
+#########################################################
 CRYPTO_MATERIALS_SRC="../../network/minimal/crypto"
 CRYPTO_MATERIALS_DST="./crypto"
 
+
+#########################################################
+# Copy crypto materials
+#########################################################
 if [ -e ${CRYPTO_MATERIALS_SRC} ]; then
   rm -rf ${CRYPTO_MATERIALS_DST}
   cp -avR ${CRYPTO_MATERIALS_SRC} ${CRYPTO_MATERIALS_DST}
 fi
 
-is_peer1_on_etc_hosts=`grep "peer1.org1" /etc/hosts | wc -l`
-if [ $is_peer1_on_etc_hosts -eq 1 ]; then
-  echo "peer1.org1 is registered on /etc/hosts"
-else
-  echo "127.0.0.1 peer1.org1" >> /etc/hosts
-fi
-
-is_orderer1_on_etc_hosts=`grep "orderer1.ordererorg" /etc/hosts | wc -l`
-if [ $is_orderer1_on_etc_hosts -eq 1 ]; then
-  echo "orderer1.ordererorg is registered on /etc/hosts"
-else
-  echo "127.0.0.1 orderer1.ordererorg" >> /etc/hosts
-fi
