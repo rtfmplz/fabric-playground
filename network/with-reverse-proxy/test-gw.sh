4606bc496520da38c23e266af96b2fbc1e7752ec
jae <kjlee.ko@gmail.com>
2019-09-02 13:45:42 +0900

    org1_gw 동작 및 네트워크 확인 테스트 추가

--- network/with-reverse-proxy/test-gw.sh
+++ network/with-reverse-proxy/test-gw.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
 # import functions
-. functions.sh
+source test-function.sh
 
-GATEWAY_ADDR="localhost:57999"
-
-###############################################################
-
-try "Example of GET and test for 404 status"
-out=$(get_conn $URL/health)
-assert "404" "$out"
+GATEWAY_IP="127.0.0.1"
+GATEWAY_ADDR="127.0.0.1:57999"
+GATEWAY_ADDR_FOR_ORDERER1="127.0.0.1:57051"
+GATEWAY_ADDR_FOR_PEER1="127.0.0.1:57050"
+GATEWAY_ADDR_FOR_PEER2="127.0.0.1:58050"
 
 ###############################################################
+# IP:PORT로 접속 테스트
+###############################################################
 
-# try "-> org1 g/w -> orderer1.ordererorg"
-# out=$(get_common_name "orderer0.ordererorg:7050" "$ORG1_GW_IP")
-# assert "orderer0" "$out"
+try "-> org1_gw"
+out=$(get_conn $GATEWAY_ADDR)
+assert "$GATEWAY_ADDR" "$out"
 
+###############################################################
+# CN(Common Name) 확인 테스트
+###############################################################
+
+try "-> org1 g/w -> orderer1.ordererorg"
+out=$(get_cn "orderer1.ordererorg:57050" "$GATEWAY_IP")
+assert "CN=orderer1.ordererorg" "$out"
+
+try "-> org1 g/w -> peer1.org1"
+out=$(get_cn "peer1.org1:57051" "$GATEWAY_IP")
+assert "CN=peer1.org1" "$out"
+
+try "-> org1 g/w -> peer2.org1"
+out=$(get_cn "peer2.org1:57051" "$GATEWAY_IP")
+assert "CN=peer2.org1" "$out"
 ###############################################################
 
 echo
