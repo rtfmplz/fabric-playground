4946ff75821695954d28a5763d8c3349e323aedb
jae <kjlee.ko@gmail.com>
2019-09-28 16:52:08 +0900

    with-reverse-proxy-tf 초기화

--- network/with-reverse-proxy-tf/ecs-key-pair.tf
+++
@@ -1,8 +0,0 @@
-##################################################
-# KEY PAIR for EC2 SSH
-##################################################
-resource "aws_key_pair" "ec2_key_pair" {
-  key_name = "${var.ec2_key_pair_name}"
-  # ssh-keygen -t rsa -b 4096 -C "kjlee.ko@gmail.com" -f "$HOME/.ssh/id_rsa" -N ""
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
