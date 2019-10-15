4b1d414b84403ddc7d828d3c90afdac1679c2bc7
jae <kjlee.ko@gmail.com>
2019-10-16 01:40:11 +0900

    aws/first-network directory 구조 작업

---
+++ aws/first-network/terraform/provider.tf
@@ -0,0 +1,8 @@
+##################################################
+# AWS only
+##################################################
+provider "aws" {
+  region     = "${var.region}"
+  #access_key = "${var.aws_access_key}"
+  #secret_key = "${var.aws_secret_key}"
+}
---
+++ aws/first-network/terraform/resources/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "init.sh start" >> /tmp/init.log
+
+echo "init.sh end" >> /tmp/init.log
