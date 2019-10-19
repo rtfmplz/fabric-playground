383b17d44be1fd3e6ae635f185c874a87ae422bc
jae <kjlee.ko@gmail.com>
2019-10-19 12:41:01 +0900

    bootstrap.sh 이 nginx 가 사용할 .env 를 생성하도록 nginx가 이를 사용하도록 함

--- aws/add-org3/bootstrap.sh
+++ aws/add-org3/bootstrap.sh
@@ -3,13 +3,26 @@
 ##############################################################
 # Variables
 ##############################################################
-HYPERLEDGER_RESOURCES_DIR="${PWD}/terraform/resources/hyperledger"
+FABRIC_RESOURCES_DIR="${PWD}/terraform/resources/hyperledger"
+NGINX_RESOURCES_DIR="${PWD}/terraform/resources/nginx"
+
 CRYPTO_CONFIG_FILE="crypto.yaml"
 ORDERER_TLS_CA_CERT_FILE="tlsca.ordererorg-cert.pem"
 FABRIC_CFG_FILE="configtx.yaml"
 CHANNEL_ARTIFACT="channel-artifact.json"
 INTERVAL=1
 DOCKER_NETWORK="hyperledger"
+
+ORDERER_ORG_HOSTNAME="orderer0"
+ORDERER_ORG_DOMAIN="ordererorg"
+HOST_ORG_DOMAIN="org1"
+
+
+HOST_ORG_GW_IP="127.0.0.1"
+if [ -z ${HOST_ORG_GW_IP} ]; then
+  echo "HOST_ORG_GW_IP is required."
+  exit 1;
+fi
 
 ORG_NAME="Org3"
 if [ -n "$1" ]; then
@@ -21,7 +34,7 @@ if [ -n "$2" ]; then
   ORG_DOMAIN=$2
 fi
 
-OUTPUT_CRYPTO_DIR="${HYPERLEDGER_RESOURCES_DIR}/crypto"
+OUTPUT_CRYPTO_DIR="${FABRIC_RESOURCES_DIR}/crypto"
 ORDERER_TLS_CA_CERT_DIR="ordererOrganizations/ordererorg/msp/tlscacerts/"
 CA_MSP_DIR="${OUTPUT_CRYPTO_DIR}/peerOrganizations/${ORG_DOMAIN}/ca/"
 
@@ -61,7 +74,7 @@ EOF
 ##############################################################
 [ -e ./${CRYPTO_CONFIG_FILE} ] && echo "${CRYPTO_CONFIG_FILE}: OK" || { echo "${CRYPTO_CONFIG_FILE} could not found."; exit 1; }
 [ -e ./${ORDERER_TLS_CA_CERT_FILE} ] && echo "${ORDERER_TLS_CA_CERT_FILE}: OK" || { echo "${ORDERER_TLS_CA_CERT_FILE} could not found."; exit 1; }
-#[ -e ./${HYPERLEDGER_RESOURCES_DIR}/.env ] && echo ".env: OK" || { echo ".env  could not found."; exit 1; }
+#[ -e ./${FABRIC_RESOURCES_DIR}/.env ] && echo ".env: OK" || { echo ".env  could not found."; exit 1; }
 [ -e ./${FABRIC_CFG_FILE} ] && echo "${FABRIC_CFG_FILE}: OK" || { echo "${FABRIC_CFG_FILE} could not found."; exit 1; }
 
 ##############################################################
@@ -88,15 +101,34 @@ configtxgen -printOrg ${ORG_NAME} > ${CHANNEL_ARTIFACT}
 sleep ${INTERVAL}
 
 ##############################################################
-# Update FABRIC_CA_SERVER_CA_KEYFILE
+# Create .env for fabric
 ##############################################################
 server_keyfile=$(tree ${CA_MSP_DIR} | grep "sk" | awk '{print $2}')
-rm -rf ${HYPERLEDGER_RESOURCES_DIR}/.env
-cat << EOF >> ${HYPERLEDGER_RESOURCES_DIR}/.env
+rm -rf ${FABRIC_RESOURCES_DIR}/.env
+cat << EOF >> ${FABRIC_RESOURCES_DIR}/.env
+ORDERER_ORG_HOSTNAME=${ORDERER_ORG_HOSTNAME}
+ORDERER_ORG_DOMAIN=${ORDERER_ORG_DOMAIN}
+HOST_ORG_DOMAIN=${HOST_ORG_DOMAIN}
 ORG_NAME=${ORG_NAME}
 ORG_DOMAIN=${ORG_DOMAIN}
 DOCKER_NETWORK=${DOCKER_NETWORK}
 FABRIC_CA_SERVER_CA_KEYFILE=${server_keyfile}
+
+EOF
+sleep ${INTERVAL}
+
+##############################################################
+# Create .env for nginx
+##############################################################
+rm -rf ${NGINX_RESOURCES_DIR}/.env
+cat << EOF >> ${NGINX_RESOURCES_DIR}/.env
+ORDERER_ORG_HOSTNAME=${ORDERER_ORG_HOSTNAME}
+ORDERER_ORG_DOMAIN=${ORDERER_ORG_DOMAIN}
+HOST_ORG_DOMAIN=${HOST_ORG_DOMAIN}
+HOST_ORG_GW_IP=${HOST_ORG_GW_IP}
+ORG_NAME=${ORG_NAME}
+ORG_DOMAIN=${ORG_DOMAIN}
+DOCKER_NETWORK=${DOCKER_NETWORK}
 
 EOF
 sleep ${INTERVAL}
