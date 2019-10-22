dc42e7c41576abfd889d1899cf47d6d4559726e4
jae <kjlee.ko@gmail.com>
2019-10-22 20:18:11 +0900

    README.md STEP 2를 작성하고 그에 맞게 add-org3/bootstrap.sh 을 수정

--- aws/README.md
+++ aws/README.md
@@ -28,7 +28,29 @@ cd first-network
 ./bootstrap.sh
 ```
 
-### output files
+### output files for first-network
 
 * tlsca.ordererorg-cert.pem
 * public-load-balancer-dns-name.org1
+
+## STEP 2. Org3-Network Bootstrap
+
+STEP 1에서 생성된 `tlsca.ordererorg-cert.pem`을 add-org3 폴더 아래 위치 시킨다.
+그리고, `public-load-balancer-dns-name.org1`안의 dns-name을 포함한 기타 환경변수를 export 한 후, bootstrpa.sh 실행
+
+```bash
+export AWS_ACCESS_KEY_ID="QWERQWERQWERQWERQWER"
+export AWS_SECRET_ACCESS_KEY="qwerqwerqwerqwerqwerqwerqwerqwerqwerqwer"
+export HOST_ENDPOINT_DNS_NAME="public-load-balancer-1d57da046f9a3e68.elb.ap-northeast-2.amazonaws.com"
+export ORG_NAME="Org3"
+export ORG_DOMAIN="org3.example.com"
+export HOST_ORG_DOMAIN="org1.example.com"
+export ORDERER_ORG_DOMAIN="ordererorg"
+cd add-org3
+./bootstrap.sh
+```
+
+### output files for add-org3
+
+* channel-artifact.json
+* public-load-balaancer-dns-name.org3
--- aws/add-org3/bootstrap.sh
+++ aws/add-org3/bootstrap.sh
@@ -3,40 +3,48 @@
 ##############################################################
 # Variables
 ##############################################################
+if [ -z ${HOST_ENDPOINT_DNS_NAME} ]; then
+  echo "HOST_ENDPOINT_DNS_NAME is required."
+  exit 1;
+fi
+
+if [ -z "${ORG_NAME}" ]; then
+  echo "ORG_NAME is required."
+  exit 1;
+fi
+
+if [ -z "${ORG_DOMAIN}" ]; then
+  echo "ORG_DOMAIN is required."
+  exit 1;
+fi
+
+if [ -z "${HOST_ORG_DOMAIN}" ]; then
+  echo "HOST_ORG_DOMAIN is required."
+  exit 1;
+fi
+
+if [ -z "${ORDERER_ORG_DOMAIN}" ]; then
+  echo "ORDERER_ORG_DOMAIN is required."
+  exit 1;
+fi
+
+[ -e ./${ORDERER_TLS_CA_CERT_FILE} ] && echo "${ORDERER_TLS_CA_CERT_FILE}: OK" || { echo "${ORDERER_TLS_CA_CERT_FILE} could not found."; exit 1; }
+
 FABRIC_RESOURCES_DIR="${PWD}/terraform/resources/hyperledger"
 NGINX_RESOURCES_DIR="${PWD}/terraform/resources/nginx"
-
 CRYPTO_CONFIG_FILE="crypto.yaml"
 ORDERER_TLS_CA_CERT_FILE="tlsca.ordererorg-cert.pem"
 FABRIC_CFG_FILE="configtx.yaml"
 CHANNEL_ARTIFACT="channel-artifact.json"
 INTERVAL=1
 DOCKER_NETWORK="hyperledger"
-
 ORDERER_ORG_HOSTNAME="orderer0"
-ORDERER_ORG_DOMAIN="ordererorg"
-HOST_ORG_DOMAIN="org1.example.com"
-
-
-HOST_ORG_GW_IP="127.0.0.1"
-if [ -z ${HOST_ORG_GW_IP} ]; then
-  echo "HOST_ORG_GW_IP is required."
-  exit 1;
-fi
-
-ORG_NAME="Org3"
-if [ -n "$1" ]; then
-  ORG_NAME=$1
-fi
-
-ORG_DOMAIN="org3.example.com"
-if [ -n "$2" ]; then
-  ORG_DOMAIN=$2
-fi
-
 OUTPUT_CRYPTO_DIR="${FABRIC_RESOURCES_DIR}/crypto"
 ORDERER_TLS_CA_CERT_DIR="ordererOrganizations/ordererorg/msp/tlscacerts/"
 CA_MSP_DIR="${OUTPUT_CRYPTO_DIR}/peerOrganizations/${ORG_DOMAIN}/ca/"
+
+export FABRIC_CFG_PATH=${PWD}
+
 
 ##############################################################
 # Preprocessing : Create resource files
@@ -73,7 +81,6 @@ EOF
 # Check resource files exist
 ##############################################################
 [ -e ./${CRYPTO_CONFIG_FILE} ] && echo "${CRYPTO_CONFIG_FILE}: OK" || { echo "${CRYPTO_CONFIG_FILE} could not found."; exit 1; }
-[ -e ./${ORDERER_TLS_CA_CERT_FILE} ] && echo "${ORDERER_TLS_CA_CERT_FILE}: OK" || { echo "${ORDERER_TLS_CA_CERT_FILE} could not found."; exit 1; }
 #[ -e ./${FABRIC_RESOURCES_DIR}/.env ] && echo ".env: OK" || { echo ".env  could not found."; exit 1; }
 [ -e ./${FABRIC_CFG_FILE} ] && echo "${FABRIC_CFG_FILE}: OK" || { echo "${FABRIC_CFG_FILE} could not found."; exit 1; }
 
@@ -91,13 +98,6 @@ sleep ${INTERVAL}
 ##############################################################
 mkdir -p ${OUTPUT_CRYPTO_DIR}/${ORDERER_TLS_CA_CERT_DIR}
 cp ${ORDERER_TLS_CA_CERT_FILE} ${OUTPUT_CRYPTO_DIR}/${ORDERER_TLS_CA_CERT_DIR}
-sleep ${INTERVAL}
-
-##############################################################
-# Create Org3 channel config
-##############################################################
-export FABRIC_CFG_PATH=${PWD}
-configtxgen -printOrg ${ORG_NAME} > ${CHANNEL_ARTIFACT}
 sleep ${INTERVAL}
 
 ##############################################################
@@ -125,7 +125,7 @@ cat << EOF >> ${NGINX_RESOURCES_DIR}/.env
 ORDERER_ORG_HOSTNAME=${ORDERER_ORG_HOSTNAME}
 ORDERER_ORG_DOMAIN=${ORDERER_ORG_DOMAIN}
 HOST_ORG_DOMAIN=${HOST_ORG_DOMAIN}
-HOST_ORG_GW_IP=${HOST_ORG_GW_IP}
+HOST_ORG_GW_IP=${HOST_ENDPOINT_DNS_NAME}
 ORG_NAME=${ORG_NAME}
 ORG_DOMAIN=${ORG_DOMAIN}
 DOCKER_NETWORK=${DOCKER_NETWORK}
@@ -134,10 +134,18 @@ EOF
 sleep ${INTERVAL}
 
 #############################################################
-# export AWS_ACCESS_KEY
+# Prepare artifacts dir
 ##############################################################
+if [ -e ./artifacts ]; then
+  rm -rf ./artifacts
+fi
+mkdir ./artifacts
 
-
+##############################################################
+# Create Org3 channel config
+##############################################################
+configtxgen -printOrg ${ORG_NAME} > ./artifacts/${CHANNEL_ARTIFACT}
+sleep ${INTERVAL}
 
 #############################################################
 # Bootstrap network
@@ -145,4 +153,5 @@ sleep ${INTERVAL}
 pushd terraform
 terraform init
 terraform apply
+echo "aws_lb.public-load-balancer.dns_name" | terraform console > ../artifacts/public-load-balancer-dns-name.org3
 popd
