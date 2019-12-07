0bb8efaf521052d7c5bd3db1c65180b6bc5fb83e
jae <kjlee.ko@gmail.com>
2019-12-08 00:02:10 +0900

    upgrade scripts

--- aws/first-network/cli-tools/create_channel.sh
+++ aws/first-network/cli-tools/create_channel.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 #############################################
-# Create Test Channel & Join
+# Create & Join Channel
 #############################################
 
-peer channel create -o orderer0.ordererorg:57050 -c ${TEST_CHANNEL_NAME} -f ${TEST_CHANNEL_NAME}.tx --tls --cafile $ORDERER_ORG_TLSCACERTS
+if [ -z $1 ]; then
+  echo "CHANNEL_NAME is ch1 (default test channel)"
+  CHANNEL_NAME=${TEST_CHANNEL_NAME}
+else
+  echo "CHANNEL_NAME is $1"
+  CHANNEL_NAME=$1
+fi
 
-peer channel join -b ${TEST_CHANNEL_NAME}.block
 
-peer channel update -o orderer0.ordererorg:57050 -c ${TEST_CHANNEL_NAME} -f ./updateAnchorOrg1.tx --tls --cafile $ORDERER_ORG_TLSCACERTS
+[ -n "${ORDERER_ORG_TLSCACERTS}" ] && echo "${ORDERER_ORG_TLSCACERTS}: OK" || { echo "ORDERER_ORG_TLSCACERTS environment is empty"; exit 1; }
+[ -e ${ORDERER_ORG_TLSCACERTS} ] && echo "${ORDERER_ORG_TLSCACERTS}: EXIST" || { echo "${ORDERER_ORG_TLSCACERTS} could not found."; exit 1; }
 
+peer channel create -o orderer0.ordererorg:57050 -c ${CHANNEL_NAME} -f ${CHANNEL_NAME}.tx --tls --cafile $ORDERER_ORG_TLSCACERTS
+
+peer channel join -b ${CHANNEL_NAME}.block
+
+peer channel update -o orderer0.ordererorg:57050 -c ${CHANNEL_NAME} -f ./updateAnchorOrg1.tx --tls --cafile $ORDERER_ORG_TLSCACERTS
 
--- aws/first-network/cli-tools/install_chaincode.sh
+++ aws/first-network/cli-tools/install_chaincode.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 #############################################
-# Test Chaincode Install & Instantiate
+# Chaincode Install & Instantiate
 #############################################
 
-peer chaincode install -n ${TEST_CHAINCODE_NAME} -v 1.0 -p github.com/chaincode/chaincode_example02/go/
+if [ -z $1 ]; then
+  echo "CHAINCODE_NAME is mycc (default test chaincode)"
+  CHAINCODE_NAME=${TEST_CHAINCODE_NAME}
+else
+  echo "CHAINCODE_NAME is $1"
+  CHAINCODE_NAME=$1
+fi
 
-peer chaincode instantiate -o orderer0.ordererorg:57050 --tls --cafile $ORDERER_ORG_TLSCACERTS -C ${TEST_CHANNEL_NAME} -n ${TEST_CHAINCODE_NAME} -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "OR ('Org1MSP.member')"
+if [ -z $2 ]; then
+  echo "CHANNEL_NAME is ch1 (default test channel)"
+  CHANNEL_NAME=${TEST_CHANNEL_NAME}
+else
+  echo "CHANNEL_NAME is $1"
+  CHANNEL_NAME=$1
+fi
+
+peer chaincode install -n ${CHAINCODE_NAME} -v 1.0 -p github.com/chaincode/chaincode_example02/go/
+
+peer chaincode instantiate -o orderer0.ordererorg:57050 --tls --cafile $ORDERER_ORG_TLSCACERTS -C ${CHANNEL_NAME} -n ${CHAINCODE_NAME} -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "OR ('Org1MSP.member')"
 
